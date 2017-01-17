7a40cd7628094ec788a7ea8f610e9d94b19b0bb0
patrickodacre <patrick@patrickwho.me>
2017-01-17 06:30:20 -0500

    initial commit

---
+++ app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Meeting;
+use App\User;
+use App\Http\Requests;
+use JWTAuth;
+
+class RegistrationController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('jwt.auth');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'meeting_id' => 'required',
+            'user_id' => 'required',
+        ]);
+        
+        $meeting_id = $request->input('meeting_id');
+        $user_id = $request->input('user_id');
+
+        // Errors will be returned if the meeting or user is not found.
+        $meeting = Meeting::findOrFail($meeting_id);
+        $user = User::findOrFail($user_id);
+
+        // Is the user already registered for the meeting?
+        if ($meeting->users()->where('users.id', $user->id)->first()) {
+            $alreadyRegisteredMsg = [
+                'msg' => 'User is already registered for the meeting',
+                'user' => $user,
+                'meeting' => $meeting,
+                'unregister' => [
+                    'href' => 'api/v1/meeting/registration' . $meeting_id,
+                    'method' => 'DELETE'
+                ]
+            ];
+
+            return response()->json($alreadyRegisteredMsg, 404);
+        }
+
+        // If the user isn't registered for the meeting already
+        $user->meetings()->attach($meeting);
+
+        $response = [
+            'msg' => 'User registered for the meeting',
+            'meeting' => $meeting,
+            'user' => $user,
+            'unregister' => 'api/v1/meeting/registration/' . $meeting->id,
+            'method' => 'DELETE'
+        ];
+
+        return response()->json($response, 201);
+    }
+    
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+
+        $meeting = Meeting::findOrFail($id);
+
+        if (!$user = JWTAuth::parseToken()->authenticate()) {
+            return response()->json(['msg' => 'User not found.'], 404);
+        }
+        
+        if (!$meeting->users()->where('users.id', $user->id)->first()) {
+            return response()->json([
+            'msg' => 'User not registered for meeting. Update not successful.'
+            ],
+            401);
+        }
+
+        $meeting->users()->detach($user->id);
+
+        $response = [
+            'msg' => 'User unregistered for meeting',
+            'meeting' => $meeting,
+            'user' => $user,
+            'register' => [
+                'href' => 'api/v1/meeting/registration',
+                'method' => 'POST',
+                'params' => 'user_id, meeting_id'
+            ]
+        ];
+
+        return response()->json($response, 200);
+    }
+}
---
+++ app/Meeting.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Meeting extends Model
+{
+
+    protected $fillable = ['time', 'title', 'description'];
+
+    public function users()
+    {
+        return $this->belongsToMany('App\User');
+    }
+}
